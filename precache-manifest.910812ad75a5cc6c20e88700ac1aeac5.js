ab37df7a9a49b55173d1cc55da3040bcc9bd9e47
Prajwal Gautam <prajwalgautam10@gmail.com>
2022-02-23 14:01:24 +0545

    Updates

---
+++ precache-manifest.910812ad75a5cc6c20e88700ac1aeac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2794d49d4d7cef8247b125f5c9f09d7a",
+    "url": "/Todo_List/index.html"
+  },
+  {
+    "revision": "d63932256b83c78c37cc",
+    "url": "/Todo_List/static/css/main.f590cb9c.chunk.css"
+  },
+  {
+    "revision": "bd38310ae2e9a2da0ef2",
+    "url": "/Todo_List/static/js/2.91bcfc47.chunk.js"
+  },
+  {
+    "revision": "d63932256b83c78c37cc",
+    "url": "/Todo_List/static/js/main.4ef00c21.chunk.js"
+  },
+  {
+    "revision": "22cdf4b448bbe6aaaacb",
+    "url": "/Todo_List/static/js/runtime~main.17693647.js"
+  }
+]);
